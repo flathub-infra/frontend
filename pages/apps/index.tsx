d40dc58b52884445aade650e7e0e484872058459
Razze <razzeee@gmail.com>
2021-10-31 13:02:06 +0100

    Use same listings code for both index pages

--- pages/apps/index.tsx
+++ pages/apps/index.tsx
@@ -1,10 +1,10 @@
 import { GetStaticProps } from 'next'
 import { Collections } from '../../src/types/Collection'
 import fetchCollection from '../../src/fetchers'
-import ApplicationSection from '../../src/components/application/Section'
 import Main from '../../src/components/layout/Main'
 import { APPS_IN_PREVIEW_COUNT } from '../../src/env'
 import { NextSeo } from 'next-seo'
+import ApplicationSections from '../../src/components/application/Sections'
 
 export default function Apps({
   recentlyUpdated,
@@ -18,33 +18,13 @@ export default function Apps({
         title='Applications'
         description='An app store and build service for Linux'
       />
-      <div className='applications-collection main-container'>
-        <div className='collection'>
-          <ApplicationSection
-            key='popular'
-            title='Popular Apps'
-            applications={popular}
-            href='/apps/collection/popular'
-          />
-          <ApplicationSection
-            key='updated'
-            title='New & Updated Apps'
-            applications={recentlyUpdated}
-            href='/apps/collection/recently-updated'
-          />
-          <ApplicationSection
-            key='editor_choice'
-            title="Editor's Choice Apps"
-            applications={editorsChoiceApps}
-            href='/apps/collection/editors-choice-apps'
-          />
-          <ApplicationSection
-            key='editor_choice_games'
-            title="Editor's Choice Games"
-            applications={editorsChoiceGames}
-            href='/apps/collection/editors-choice-games'
-          />
-        </div>
+      <div className='main-container'>
+        <ApplicationSections
+          popular={popular}
+          recentlyUpdated={recentlyUpdated}
+          editorsChoiceApps={editorsChoiceApps}
+          editorsChoiceGames={editorsChoiceGames}
+        ></ApplicationSections>
       </div>
     </Main>
   )
