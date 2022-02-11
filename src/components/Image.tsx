a72dcb4ffd3699f649672193ef213efdceb3c30d
Kolja Lampe <klampe@slashwhy.de>
2022-02-11 15:13:30 +0100

    Type ImageProps

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,15 +1,10 @@
-import NextImage from "next/image";
+import NextImage, { ImageProps } from 'next/image'
 
 // opt-out of image optimization, no-op
 const customLoader = ({ src }) => {
   return src
 }
 
-export default function Image(props) {
-  return (
-    <NextImage
-      {...props}
-      loader={customLoader}
-    />
-  );
+export default function Image(props: ImageProps) {
+  return <NextImage {...props} loader={customLoader} />
 }
